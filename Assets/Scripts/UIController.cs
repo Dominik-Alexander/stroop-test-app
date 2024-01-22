9c6c96a84780c709daed2d4a89eba6ef8e615e2b
Dominik Szczepek <DSzczepek@outlook.de>
2024-01-22 22:06:58 +0100

    Add project

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+// TODO: Clean up all this mess and find the bug.
+
+public class UIController : MonoBehaviour
+{
+    VisualElement root;
+
+    VisualElement veLabelContainer;
+    Label lbColorWord;
+
+    VisualElement veButtonContainer;
+    Button btGreen;
+    Button btRed;
+    Button btBlue;
+    Button btYellow;
+
+    enum ColorWordColor {
+        WHITE,
+        GREEN,
+        RED,
+        BLUE,
+        YELLOW
+    };
+
+    enum ColorWordText
+    {
+        DOT,
+        GREEN,
+        RED,
+        BLUE,
+        YELLOW
+    };
+
+    ColorWordColor colorWordColor;
+    ColorWordText colorWordText;
+
+    int randomizedStorage = 0;
+    bool running = false;
+    int numIter = 0;
+
+    int userInput = 0;
+    bool correctlyAnswered = false;
+    int userScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        colorWordColor = ColorWordColor.WHITE;
+        colorWordText = ColorWordText.DOT;
+
+        root = GetComponent<UIDocument>().rootVisualElement;
+
+        veLabelContainer = root.Q<VisualElement>("VELabelContainer");
+        lbColorWord = veLabelContainer.Q<Label>("LbColorWord");
+
+        veButtonContainer = root.Q<VisualElement>("VEButtonContainer");
+        btGreen = veButtonContainer.Q<Button>("BtGreen");
+        btRed = veButtonContainer.Q<Button>("BtRed");
+        btBlue = veButtonContainer.Q<Button>("BtBlue");
+        btYellow = veButtonContainer.Q<Button>("BtYellow");
+
+        running = true;
+
+        btGreen.clicked += GreenClicked;
+        btRed.clicked += RedClicked;
+        btBlue.clicked += BlueClicked;
+        btYellow.clicked += YellowClicked;
+
+        StartCoroutine(ChangeColorAndTextRoutine());
+    }
+
+    IEnumerator ChangeColorAndTextRoutine()
+    {
+        while (running)
+        {
+            correctlyAnswered = false;
+
+            ++numIter;
+
+            if(numIter % 4 == 0)
+            {
+                randomizedStorage = Random.Range(1, 5);
+                ChangeColor(randomizedStorage);
+                ChangeText(randomizedStorage);
+            } else
+            {
+                ChangeColor(Random.Range(1, 5));
+                ChangeText(Random.Range(1, 5));
+            }
+
+            lbColorWord.text = colorWordText.ToString();
+
+            switch (colorWordColor)
+            {
+                case 0:
+                    lbColorWord.style.color = Color.white;
+                    break;
+                case (ColorWordColor)1:
+                    lbColorWord.style.color = Color.green;
+                    break;
+                case (ColorWordColor)2:
+                    lbColorWord.style.color = Color.red;
+                    break;
+                case (ColorWordColor)3:
+                    lbColorWord.style.color = Color.blue;
+                    break;
+                case (ColorWordColor)4:
+                    lbColorWord.style.color = Color.yellow;
+                    break;
+            }
+
+            if (userInput == (int)colorWordColor && colorWordColor != 0)
+            {
+                correctlyAnswered = true;
+            }
+
+            yield return new WaitForSeconds(2.0f);
+
+            lbColorWord.style.color = Color.white;
+            lbColorWord.text = ".";
+
+            if(correctlyAnswered)
+            {
+                ++userScore;
+            }
+
+            Debug.Log(userScore);
+
+            yield return new WaitForSeconds(0.75f);
+
+            if(numIter == 15)
+            {
+                running = false;
+            }
+        }
+    }
+
+    private void ChangeColor(int colorNr)
+    {
+        colorWordColor = (ColorWordColor)colorNr;
+    }
+
+
+    private void ChangeText(int textNr)
+    {
+        colorWordText = (ColorWordText)textNr;
+    }
+
+    private void GreenClicked()
+    {
+        userInput = 1;
+    }
+
+    private void RedClicked()
+    {
+        userInput = 2;
+    }
+
+    private void BlueClicked()
+    {
+        userInput = 3;
+    }
+
+    private void YellowClicked()
+    {
+        userInput = 4;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# stroop-test-app
-The stroop test
+# About the Stroop Test
+
+The Stroop Test is a psychological assessment that measures cognitive processing speed and selective attention. In this test, individuals are presented with a list of color words (e.g., red, blue, green) written in different ink colors. The challenge arises when the ink color does not match the written color word (e.g., the word "red" written in blue ink). Participants are required to name the ink color while inhibiting the automatic response to read the word. The interference between the conflicting information (word and ink color) creates a cognitive conflict, making it more difficult for individuals to accurately and quickly identify the ink color. The Stroop Test is widely used in psychology to evaluate executive functions, such as cognitive flexibility and response inhibition. Differences in performance can provide insights into various cognitive processes and are often utilized in clinical assessments and research studies.
